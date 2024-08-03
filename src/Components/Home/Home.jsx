516aca8638ecc51014622d4a97df9c75f53d443c
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-04 00:10:12 +0300

    salom

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import "./Home.css"
 import axios from 'axios';
-import { Table } from 'antd';
+import { Button, Table } from 'antd';
 const Home = () => {
     const [cities,setCities] = useState([]);
     const getCities = () =>{
         axios.get('https://autoapi.dezinfeksiyatashkent.uz/api/cities')
-        .then(res=setCities(res.data.data))
+        .then(res=>setCities(res.data.data))
         .catch(err=>console.log(err))
     }
-    useEffect(()=>[
+    useEffect(()=>{
         getCities()
-    ],[])
+     },[])
     const columns = [
+        {
+            title: 'Number',
+            dataIndex: 'number',
+        },
         {
             title: 'Name',
             dataIndex: 'name',
@@ -24,8 +28,26 @@ const Home = () => {
         {
             title: 'Images',
             dataIndex: 'images',
+        },
+        {
+            title:'Action',
+            dataIndex:'action',
         }
     ]
+    const data = cities.map((city,index)=>(
+        {
+            number:index+1,
+            key:index,
+            name:city.name,
+            text:city.text,
+            images: (
+                <img width={150}
+                    src={`https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/${city.image_src}`} 
+                    alt={city.name} 
+                />),
+            car:(<><Button type='primary' primary>Edit</Button> <Button type='primary' danger>Delete</Button></>)
+        }
+    ))
   return (
     <div>
         <Table columns={columns} dataSource={data}/>
