a246a230ebbc374db37240ef8daa79b97616dd48
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-07 22:06:42 +0300

    Salom

---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Menu.css"
+const Menu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Menu
--- src/Components/Router/router.jsx
+++ src/Components/Router/router.jsx
@@ -2,8 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home/Home";
 import App from "../../App";
 import Model from "../Model/Model";
-
-
+import Menu from "../Menu/Menu";
 
 const router = createBrowserRouter([
     {
@@ -17,6 +16,10 @@ const router = createBrowserRouter([
     {
         path: "/model",
         element: <Model/>,
+    },
+    {
+        path: "/menu",
+        element: <Menu/>,
     }
 ])
 export default router
