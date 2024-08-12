19089cfbdd111f5d3b5c1645150eb5b470db9b1f
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-12 20:31:45 +0300

    salom

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 import "./Brands.css"
+import axios from 'axios'
 const Brands = () => {
-    const [brands,setBrands] = useState(" ")
+    const [brands, setBrands] = useState([])
+    const getBrands = () => {
+        axios.get('https://autoapi.dezinfeksiyatashkent.uz/api/brands')
+        .then(res=>setBrands(res.data.data))
+    }
   return (
     <div>
-      
+
     </div>
   )
 }
