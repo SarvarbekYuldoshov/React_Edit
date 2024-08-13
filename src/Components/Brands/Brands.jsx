1a97451ebc5cf1f8f109a2b00502fa46c37d6769
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-13 06:40:12 +0300

    salom

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -1,27 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import "./Brands.css"
-import axios from 'axios'
-import Item from 'antd/es/list/Item'
+import React, { useEffect, useState } from 'react';
+import "./Brands.css";
+import axios from 'axios';
+
 const Brands = () => {
-    const [brands, setBrands] = useState([])
+    const [brands, setBrands] = useState([]);
+
     const getBrands = () => {
         axios.get('https://autoapi.dezinfeksiyatashkent.uz/api/brands')
-        .then(res=>setBrands(res.data.data))
-    }
-    useEffect(()=>{
-        getBrands()
-    },[])
-  return (
-    <div>
-       <h1>Brands</h1>
-       {
-        brands && brands.map((Item,index)=>({
-            
-        }
-        ))
-       }
-    </div>
-  )
-}
+        .then(res => setBrands(res.data.data))
+        .catch(error => console.error('Error fetching brands:', error)); // Optional error handling
+    };
 
-export default Brands
+    useEffect(() => {
+        getBrands();
+    }, []);
+
+    return (
+        <div>
+            <h1>Brands</h1>
+            {
+                brands && brands.map((brand, index) => (
+                    <div key={index} className="brand-item">
+                        <h2>{brand.id}</h2>
+                    </div>
+                ))
+            }
+        </div>
+    );
+};
+
+export default Brands;
+
+
+
+
